df44871fd2cc4212932797a20d04cd0c26b1a386
KebSummers <summerskeb@gmail.com>
2023-11-18 21:13:19 -0700

    Dont really think the dropdown parts for each button work but thats for another day

--- BuilderScene/satellite-lab.gd
+++ BuilderScene/satellite-lab.gd
@@ -4,48 +4,79 @@ extends Node2D
 var satellite_parts = {}  # Holds the actual part instances
 var available_parts = {}  # Holds the metadata from the JSON file
 
+# Keep track of the currently visible VBoxContainer
+var current_visible_vbox = null  
+
+# Import the File module
+var File = preload("res://DesignScene/all_parts.json")
+
 func _ready():
 	initialize_parts()
 	update_ui()
 
 func initialize_parts():
 	var file = File.new()
-	if file.open("res://path_to_your_file/parts.json", File.READ) == OK:
-		var data = parse_json(file.get_as_text())
+	if file.open("res://DesignScene/all_parts.json", File.READ) == OK:
+		var json_text = file.get_as_text()
 		file.close()
+		
+		var json = JSON.new()
+		var error = json.parse(json_text)
+		if error == OK:
+			var data = json.get_data()  # This is the parsed JSON data
 
-		# Create instances of parts based on the JSON data
-		for category in data.keys():
-			available_parts[category] = []
-			for part_data in data[category]:
-				var part = create_part_instance(category, part_data)
-				available_parts[category].append(part)
+			# Create instances of parts based on the JSON data
+			for category in data.keys():
+				available_parts[category] = []
+				var category_vbox = get_node("Categories/" + category + "/" + category)
+				category_vbox.queue_free() # Clear previous buttons if any
+				var new_category_vbox = VBoxContainer.new() # Create a new VBoxContainer
+				new_category_vbox.name = category
+				
+				for part_data in data[category]:
+					var part = create_part_instance(category, part_data)
+					available_parts[category].append(part)
+					
+					# Create a button for the part
+					var part_button = Button.new()
+					part_button.text = part_data["name"]
+					part_button.call_deferred("pressed", self, "_on_PartButton_pressed", [part_button, category, part])
+					
+					# Add the button to the VBoxContainer
+					new_category_vbox.add_child(part_button)
+				
+				# Add the new VBoxContainer to the scene
+				get_node("Categories/" + category).add_child(new_category_vbox)
+		else:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", json_text, " at line ", json.get_error_line())
 
 func create_part_instance(category: String, part_data: Dictionary) -> SatellitePart:
 	var part
 	match category:
 		"PowerSystem":
-			part = PowerSystemPart.new(
-				part_data["name"],
-				part_data["weight"],
-				part_data["cost"],
-				part_data["compatibility"],
-				# ... add all specific attributes here ...
-			)
-		# ... match other
+			part = PowerSystemPart.new(part_data)
+		"Communications":
+			part = CommunicationsPart.new(part_data)
+		"Payload":
+			part = PayloadPart.new(part_data)
+		"Propulsion":
+			part = PropulsionPart.new(part_data)
+		"SpacecraftBus":
+			part = SpacecraftBusPart.new(part_data)
+		_:
+			print("Unknown category: %s" % category)
+	return part
 
 func update_ui():
 	# Reset all VBoxContainers to be hidden
 	for vbox in get_node("Categories").get_children():
-		vbox.hide()
-	# Hide currently visible vbox if any
-	if current_visible_vbox:
-		current_visible_vbox.hide()
+		for category_vbox in vbox.get_children():
+			category_vbox.hide()
 	pass
 
 func show_components_for_category(category):
 	update_ui() # Hide all vboxes
-	var vbox = get_node("Categories/" + category)
+	var vbox = get_node("Categories/" + category + "/" + category)
 	vbox.show()
 	current_visible_vbox = vbox
 
@@ -72,5 +103,9 @@ func select_part_button(button: Button, category: String):
 	button.get_child(0).add_color_override("font_color", Color(0.5, 0.5, 0.5)) # Gray text for selected
 
 func _on_ComponentButton_pressed(button: Button, category: String, part: SatellitePart):
+	select_part_button(button, category)
+	add_part_to_satellite(category, part)
+	
+func _on_PartButton_pressed(button: Button, category: String, part: SatellitePart):
 	select_part_button(button, category)
 	add_part_to_satellite(category, part)
--- PowerSystemPart.gd
+++ PowerSystemPart.gd
@@ -10,12 +10,19 @@ var voltage_regulation = false
 var power_consumption = 0
 var energy_harvesting = false
 
-func _init(_part_name, _weight, _cost, _compatibility, _power_source, _solar_panel_efficiency, _battery_capacity, _battery_type, _voltage_regulation, _power_consumption, _energy_harvesting):
-	super._init(_part_name, _weight, _cost, _compatibility)  # Call the parent constructor
-	power_source = _power_source
-	solar_panel_efficiency = _solar_panel_efficiency
-	battery_capacity = _battery_capacity
-	battery_type = _battery_type
-	voltage_regulation = _voltage_regulation
-	power_consumption = _power_consumption
-	energy_harvesting = _energy_harvesting
+func _init(part_data: Dictionary):
+	# Extract values from the part_data dictionary
+	var _part_name = part_data["part_name"]
+	var _weight = part_data["weight"]
+	var _cost = part_data["cost"]
+	var _compatibility = part_data["compatibility"]
+	power_source = part_data["power_source"]
+	solar_panel_efficiency = part_data["solar_panel_efficiency"]
+	battery_capacity = part_data["battery_capacity"]
+	battery_type = part_data["battery_type"]
+	voltage_regulation = part_data["voltage_regulation"]
+	power_consumption = part_data["power_consumption"]
+	energy_harvesting = part_data["energy_harvesting"]
+
+	# Call the parent constructor
+	super._init(_part_name, _weight, _cost, _compatibility)
