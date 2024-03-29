7ad8d542a186fe9284ee57b1a9edd14aac0aeb10
KebSummers <summerskeb@gmail.com>
2024-03-28 20:26:35 -0700

    stop

--- Global.gd
+++ Global.gd
@@ -1,7 +1,6 @@
 # Global.gd
 extends Node
 
-<<<<<<< HEAD
 # Preload the SatellitePart script file
 const SatelliteParts = preload("res://Classes/SatellitePart.gd")
 
@@ -29,9 +28,3 @@ func _init():
 #	current_satellite.add_part(power_part)
 #	current_satellite.add_part(propulsion_part)
 #	current_satellite.add_part(bus_part)
-=======
-var input_allowed = true
-
-func set_input_allowed(allowed):
-	input_allowed = allowed
->>>>>>> ae7630d24d24048725c7f68d3669b3558278e589
