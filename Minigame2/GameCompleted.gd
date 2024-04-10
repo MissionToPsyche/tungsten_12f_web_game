1129ffaf809545faec41fe5e957be11fbb07754b
SukulH <71863812+SukulH@users.noreply.github.com>
2024-04-09 18:06:45 -0700

    Update GameCompleted.gd
    score updated to global variable

--- Minigame2/GameCompleted.gd
+++ Minigame2/GameCompleted.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var finalScore: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,8 +11,15 @@ func _process(delta):
 	pass
 
 func DisplayScore(score: int):
+		
 	$Score.text = "Your Score: " + str(score)
+	finalScore = score
+	
 
 
 func _on_texture_button_pressed():
+	var global_node = get_node("/root/Global")
+	global_node.score = global_node.score + finalScore
+	print(global_node.score)
 	get_tree().change_scene_to_file("res://DecesionTreeScene/decision_tree.tscn")
+	
