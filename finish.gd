cb912cf45194d3abaaa9bc3e6ae83d1d95754abf
gchallar <118143817+gchallar@users.noreply.github.com>
2024-04-11 10:21:31 -0700

    Change controls and add points to global score

--- finish.gd
+++ finish.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
+var manager_instance
+
+func _ready():
+	manager_instance = preload("res://game_manager.gd").new()
+
 func _on_body_entered(body):
 	if(body.name =="Player"):
 		get_tree().change_scene_to_file("res://DecesionTreeScene/decision_tree.tscn")
-
+		manager_instance.transfer_points_to_global()
