76106121c7b3c2f02e917cfac51f9df9a30912e4
gchallar <118143817+gchallar@users.noreply.github.com>
2024-04-11 10:41:14 -0700

    movement controls and global score update

--- finish.gd
+++ finish.gd
@@ -3,4 +3,6 @@ extends Area2D
 func _on_body_entered(body):
 	if(body.name =="Player"):
 		get_tree().change_scene_to_file("res://DecesionTreeScene/decision_tree.tscn")
+		var final_score = int($UI/Panel/PointsLabel.text)
+		Global.score += final_score
 
--- game_manager.gd
+++ game_manager.gd
@@ -7,12 +7,14 @@ var points = 0
 func add_point(): 
 	points += 5 
 	print(points)
-	points_label.text = "POINTS:" + str(points) 
+	#points_label.text = "POINTS:" + str(points) 
+	points_label.text = str(points)
 	
 	
 func lose_points(): 
 	points -= 3 
 	print(points)
-	points_label.text = "POINTS:" + str(points)
+	#points_label.text = "POINTS:" + str(points)
+	points_label.text = str(points)
 
 
